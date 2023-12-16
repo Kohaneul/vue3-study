53c21a65d75f51d84c17709e92bb50ccf229419e
hanul1106 <hanul1106@nearsolution.co.kr>
2023-12-16 21:14:13 +0900

    Initial commit

---
+++ src/lecture/4/App_CompositionAPI.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<h2>반응형 메세지</h2>
+		<h2>{{ reactiveMessage }}</h2>
+		<button v-on:click="addReactiveMessage">add Message</button>
+		<h2>일반 메세지</h2>
+		<h2>{{ normalMessage }}</h2>
+		<button v-on:click="addNormalMessage">add Message</button>
+	</div>
+</template>
+
+<script>
+import { isRef, ref, onMounted, onBeforeMount } from 'vue';
+export default {
+	setup() {
+		console.log('setUp');
+		const reactiveMessage = ref('reactive Message');
+		const normalMessage = 'Hello Message';
+		const addReactiveMessage = () => {
+			reactiveMessage.value = reactiveMessage.value + '!';
+		};
+		// console.log('isRef(reactiveMessage)', isRef(reactiveMessage));
+
+		let addNormalMessage = () => {
+			normalMessage.value = normalMessage.value + '!';
+		};
+		// console.log('isRef(normalMessage)', isRef(normalMessage));
+		onMounted(() => {
+			console.log('onMounted');
+		});
+
+		onBeforeMount(() => {
+			console.log('onBeforeMount');
+		});
+		return {
+			reactiveMessage,
+			normalMessage,
+			addReactiveMessage,
+			addNormalMessage,
+		};
+	},
+};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,15 @@
+<template>
+	<h2>About View</h2>
+	<p>{{ $route.path }}</p>
+	<button class="btn btn-primary" @click="goAboutPage()">Home으로 이동</button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+const router = useRouter();
+const goAboutPage = () => {
+	router.push('/');
+};
+</script>
+
+<style lang="scss" scoped></style>
