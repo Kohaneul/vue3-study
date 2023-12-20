1835ec131b712ae080092b8bf3d4fdd534ced793
hanul1106 <hanul1106@nearsolution.co.kr>
2023-12-20 09:22:28 +0900

    1220 commit

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,26 @@
 <template>
-	<!-- <div class="text" :class="{ active: isActive, 'text-danger': hasError }"> -->
-	<div class="text" :class="[activeClass, errorClass, classObject]">
-		텍스트 입니다11
-	</div>
-	<div class="text" :class="isActive ? 'error' : 'class'">텍스트 입니다</div>
-
-	<button @click="toggle">toggle</button>
-	<button @click="hasError = !hasError">toggleError</button>
+	<div :style="{}">afjewfjalefjlawefjlawelfkawfkjle</div>
 </template>
 
 <script>
-import { reactive, computed, ref } from 'vue';
+import { computed, reactive } from 'vue';
 export default {
 	setup() {
-		const isActive = ref(true);
-		const hasError = ref(false);
-		const activeClass = ref('active');
-		const errorClass = ref('error');
-		const toggle = () => {
-			isActive.value = !isActive.value;
-		};
-		const toggle2 = () => {
-			hasError.value = !hasError.value;
-		};
-		const classObject = computed(() => {
-			return {
-				active: true && true,
-				'text-object': true && true,
-				'text-blue': true,
-			};
+		// const styleObject = reactive({
+		// 	color: 'red',
+		// 	fontSize: '13px',
+		// });
+		const styleObject = computed(()=>{
+	return {
+		color: 'red',
+			fontSize: '13px',
 		});
+		
+		})
 
-		return {
-			isActive,
-			hasError,
-			classObject,
-			toggle,
-			toggle2,
-			activeClass,
-			errorClass,
-		};
+		return { styleObject };
 	},
 };
 </script>
 
-<style scoped>
-.active {
-	font-weight: 900;
-}
-.error {
-	color: red;
-}
-.text-danger {
-	color: red;
-}
-</style>
+<style scoped></style>
