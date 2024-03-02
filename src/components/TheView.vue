9ef303f6a248e2ec9dbb3bac6d9f9ccede61a7e6
hanul1106 <hanul1106@nearsolution.co.kr>
2024-03-02 19:22:26 +0900

    0302 new study

---
+++ src/components/TheView.vue
@@ -0,0 +1,40 @@
+<template>
+  <main>
+	<div class="container py-4">
+		<div class="row g-3">
+    <div class="col col-4">
+			<AppCard></AppCard>
+		</div>
+		<div class="col col-4">
+			<AppCard></AppCard>
+		</div>   
+		<div class="col col-4">
+			<AppCard></AppCard>
+		</div>
+		<div class="col col-4">
+			<AppCard></AppCard>
+		</div>
+		<div class="col col-4">
+			<AppCard></AppCard>
+		</div>
+		<div class="col col-4">
+			<AppCard></AppCard>
+		</div>
+  </div>
+	</div>
+</main>
+</template>
+
+<script>
+console.log('AppCard')
+import AppCard from '@/components/AppCard.vue';
+  export default {
+    components : {
+			AppCard,
+		}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
